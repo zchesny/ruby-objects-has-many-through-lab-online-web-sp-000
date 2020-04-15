d4d9601449d0168298596835e2cc33e49b1039fc
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 18:28:07 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select{|song| song.artist == self}
+  end
+
+  def genres
+    self.songs.collect{|song| song.genre}.uniq
+  end
+
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
+
+end

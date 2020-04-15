de56a2a5ccd72672927124d34d6d67bfdcd4914c
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 18:22:38 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,24 @@
+class Artist
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Songs.all.select{|song| song.artist == self}
+  end
+
+  def genres
+    self.songs.collect{|song| song.genre}.uniq
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+def Genre
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all.select{|song| song.genre == self}
+  end
+
+  def artists
+    self.songs.collect{|song| song.artist}.uniq
+  end
+
+end
